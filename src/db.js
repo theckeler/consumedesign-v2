c0a2aed3998b7ffe14822e0933b2a215601402cf
Todd Heckeler <todd@consumedesign.com>
2021-10-06 14:33:50 -0400

    adding db for now

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const connection = {
+  baseURL: "https://admin.consumedesign.com/graphql",
+  username: "theckeler",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "bearer xxxxxxxxxxx",
+  },
+};
+
+export default connection;
